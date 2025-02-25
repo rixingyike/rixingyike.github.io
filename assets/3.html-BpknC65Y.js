7107603b271c193067869d003df803f156e59f69
rixingyike <9830131@qq.com>
2025-02-25 00:13:43 +0000

    Update from https://github.com/rixingyike/yishulun_blog_mdandcode/commit/10a8feef782148f4a27f90d0262345fbb278e912

---
+++ assets/3.html-BpknC65Y.js
@@ -0,0 +1,28 @@
+import{_ as n,c as a,b as e,o as i}from"./app-dG2nJzry.js";const l="/assets/image-20250225075353882-CzpskyLN.png",p={};function c(d,s){return i(),a("div",null,s[0]||(s[0]=[e(`<div class="language-text line-numbers-mode" data-ext="text" data-title="text"><button class="copy" title="复制代码" data-copied="已复制"></button><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>杯中倒悬的山河，乃我眼中凝眸的烈焰</span></span>
+<span class="line"><span>嘶——</span></span>
+<span class="line"><span>拉格拉开的刹那，喉咙的闸门被叫开了</span></span>
+<span class="line"><span>五岳燃成灰烬，仿佛还要轻些</span></span>
+<span class="line"><span>我甩开屁股下面的椅子</span></span>
+<span class="line"><span>跳舞，开始跳舞</span></span>
+<span class="line"><span>影子狂躁零乱，似要撞碎墙壁</span></span>
+<span class="line"><span>月光把地砖敲成战鼓，当当当响</span></span>
+<span class="line"><span>酒瓶滚过街道，掩过整座城市的叹息</span></span>
+<span class="line"><span>长江</span></span>
+<span class="line"><span>如同我体内的血管，在涨潮——</span></span>
+<span class="line"><span>浪头汹汹拍打在我的胸膛</span></span>
+<span class="line"><span>黄河！</span></span>
+<span class="line"><span>似乎咆哮着，被我倒进了酒杯</span></span>
+<span class="line"><span>我凝视着酒杯，这一眸万年</span></span>
+<span class="line"><span>仰头饮下这杯泥沙与雷鸣——</span></span>
+<span class="line"><span>要吼就吼个痛快吧！</span></span>
+<span class="line"><span>黄河，我让长江让给你半把椅子</span></span>
+<span class="line"><span>尽情在我的血管里奔腾吧</span></span>
+<span class="line"><span>别学那秋风呜咽，也别学那冬雷闪电</span></span>
+<span class="line"><span>八十岁的马蹄，要奔腾，奔腾——</span></span>
+<span class="line"><span>踏得地平线发烫</span></span>
+<span class="line"><span>天际的霓红，也不过是我仰头干杯时</span></span>
+<span class="line"><span>从咽喉滚落的，一粒粒麦芒</span></span>
+<span class="line"><span>今夜，我的脊梁，在酒水里淬火，锻成钢</span></span>
+<span class="line"><span>当月光钻进我的衣兜，空荡荡</span></span>
+<span class="line"><span>我听见骨头，在黑暗中，在黎明前</span></span>
+<span class="line"><span>一节节拔高，铮铮作响</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><blockquote><p>昨天晚上喝了一杯拉格，感觉好像还可以，像是一种又便宜又好喝的酒。迷迷糊糊中涂了此篇，不知所云，今天早上整理一下。</p></blockquote><p>2025年2月25日</p><p><img src="`+l+'" alt="image-20250225075353882"></p>',4)]))}const r=n(p,[["render",c],["__file","3.html.vue"]]),v=JSON.parse('{"path":"/blog/2025/3.html","title":"杯中倒悬的山河","lang":"zh-CN","frontmatter":{"createTime":"2025/02/25 20:25:00","tags":["诗歌"],"title":"杯中倒悬的山河"},"headers":[],"readingTime":{"minutes":1.26,"words":377},"git":{"updatedTime":1740442286000,"contributors":[{"name":"LIYI","username":"LIYI","email":"9830131@qq.com","commits":2,"avatar":"https://avatars.githubusercontent.com/LIYI?v=4","url":"https://github.com/LIYI"}]},"filePathRelative":"blog/2025/3.md","categoryList":[{"id":"126ac9","sort":10005,"name":"blog"},{"id":"a264e0","sort":10006,"name":"2025"}]}');export{r as comp,v as data};
